94adf55ebc9d4fbf331f57cf81e6b9a3eb7a63d7
Ketrin <ketrin632@gmail.com>
2020-07-05 18:40:25 +0300

    second commit

--- Shape.cpp
+++ Shape.cpp
@@ -1,1 +1,186 @@
 #include "Shape.h"
+
+void Square::Show() const
+{
+	cout << "\t**Square**" << endl;
+	cout << "Coords of the upper left corner: x=" << x << ", y=" << y << endl;
+	cout << "Side of the square: a=" << a << endl;
+}
+
+void Square::Save(string path)
+{
+	ofstream fout;
+	fout.open(path);
+	bool isOpen = fout.is_open();
+	if (isOpen == true) {
+
+		fout << x << endl;
+		fout << y << endl;
+		fout << a << endl;
+	}
+	else {
+		cout << "Error!" << endl;
+	}
+
+	fout.close();
+}
+
+void Square::Load(string path)
+{
+	ifstream fin;
+	fin.open(path);
+	bool isOpen = fin.is_open();
+	if (isOpen == true) {
+		fin >> x;
+		fin >> y;
+		fin >> a;
+	}
+	else
+	{
+		cout << "Error!" << endl;
+	}
+
+	fin.close();
+}
+
+void Circle::Show() const
+{
+	cout << "\t**Circle**" << endl;
+	cout << "Coords of the of the center: x=" << x << ", y=" << y << endl;
+	cout << "Radius of the circle: a=" << r << endl;
+}
+
+void Circle::Save(string path)
+{
+	ofstream fout;
+	fout.open(path);
+	bool isOpen = fout.is_open();
+	if (isOpen == true) {
+
+		fout << x << endl;
+		fout << y << endl;
+		fout << r << endl;
+	}
+	else {
+		cout << "Error!" << endl;
+	}
+
+	fout.close();
+}
+
+void Circle::Load(string path)
+{
+	ifstream fin;
+	fin.open(path);
+	bool isOpen = fin.is_open();
+	if (isOpen == true) {
+
+		fin >> x;
+		fin >> y;
+		fin >> r;
+
+	}
+	else
+	{
+		cout << "Error!" << endl;
+	}
+
+	fin.close();
+}
+
+void Rectangle::Show() const
+{
+	cout << "\t**Rectangle**" << endl;
+	cout << "Coords of the upper left corner: x=" << x << ", y=" << y << endl;
+	cout << "Sides of the rectangle: a=" << a << ", b=" << b << endl;
+}
+
+void Rectangle::Save(string path)
+{
+	ofstream fout;
+	fout.open(path);
+	bool isOpen = fout.is_open();
+	if (isOpen == true) {
+
+		fout << x << endl;
+		fout << y << endl;
+		fout << a << endl;
+		fout << b << endl;
+
+	}
+	else {
+		cout << "Error!" << endl;
+	}
+
+	fout.close();
+}
+
+void Rectangle::Load(string path)
+{
+	ifstream fin;
+	fin.open(path);
+	bool isOpen = fin.is_open();
+	if (isOpen == true) {
+
+		fin >> x;
+		fin >> y;
+		fin >> a;
+		fin >> b;
+
+	}
+	else
+	{
+		cout << "Error!" << endl;
+	}
+
+	fin.close();
+}
+
+void Ellipse::Show() const
+{
+	cout << "\t**Ellipse**" << endl;
+	cout << "Coords of the upper left corner of the rectangle circumscribed around the ellipse: x=" <<
+		x << ", y=" << y << endl;
+	cout << "Sides of the rectangle: a=" << a << ", b=" << b << endl;
+}
+
+void Ellipse::Save(string path)
+{
+	ofstream fout;
+	fout.open(path);
+	bool isOpen = fout.is_open();
+	if (isOpen == true) {
+
+		fout << x << endl;
+		fout << y << endl;
+		fout << a << endl;
+		fout << b << endl;
+
+	}
+	else {
+		cout << "Error!" << endl;
+	}
+
+	fout.close();
+}
+
+void Ellipse::Load(string path)
+{
+	ifstream fin;
+	fin.open(path);
+	bool isOpen = fin.is_open();
+	if (isOpen == true) {
+
+		fin >> x;
+		fin >> y;
+		fin >> a;
+		fin >> b;
+
+	}
+	else
+	{
+		cout << "Error!" << endl;
+	}
+
+	fin.close();
+}
--- Shape.h
+++ Shape.h
@@ -12,7 +12,6 @@ public:
 	virtual void Save(string path)  = 0;
 	
 	virtual void Load(string path)  = 0;
-
 };
 
 class Square : public Shape {
@@ -22,50 +21,13 @@ private:
 	int a;
 public:
 	Square() : x(0), y(0), a(0) {}
-	Square(int x, int y, int a) : x(x), y(y), a(a) {}
+	Square(int x, int y, int a) : x(x), y(y), a(a) {}	
 	
-	
-	virtual void Show() const {
+	virtual void Show() const;
 
-		cout << "\t**Square**" << endl;
-		cout << "Coords of the upper left corner: x=" << x << ", y=" << y << endl;
-		cout << "Side of the square: a=" << a << endl;
-	}
+	virtual void Save(string path) override;	
 
-	virtual void Save(string path) override
-	{	
-		ofstream fout;
-		fout.open(path);
-		bool isOpen = fout.is_open();
-		if (isOpen == true) {
-
-			fout << x << endl;
-			fout << y << endl;
-			fout << a << endl;
-		}
-		else {
-			cout << "Error!" << endl;
-		}
-
-		fout.close();
-	}
-
-	virtual void Load(string path) override {
-		ifstream fin;
-		fin.open(path);
-		bool isOpen = fin.is_open();
-		if (isOpen == true) {			
-				fin >> x;
-				fin >> y;
-				fin >> a;			
-		}
-		else
-		{
-			cout << "Error!" << endl;
-		}
-
-		fin.close();	
-	}
+	virtual void Load(string path) override;
 };
 
 
@@ -79,49 +41,11 @@ public:
 	Circle() : x(0), y(0), r(0) {}
 	Circle(int x, int y, int r) : x(x), y(y), r(r) {}
 
-	virtual void Show() const {
-		cout << "\t**Circle**" << endl;
-		cout << "Coords of the of the center: x=" << x << ", y=" << y << endl;
-		cout << "Radius of the circle: a=" << r << endl;
-	}
+	virtual void Show() const;
 
-	virtual void Save(string path)  override {
-		ofstream fout;
-		fout.open(path);
-		bool isOpen = fout.is_open();
-		if (isOpen == true) {
+	virtual void Save(string path)  override;
 
-			fout << x << endl;
-			fout << y << endl;
-			fout << r << endl;
-		}
-		else {
-			cout << "Error!" << endl;
-		}
-
-		fout.close();
-	
-	}
-
-	virtual void Load(string path) {
-		ifstream fin;
-		fin.open(path);
-		bool isOpen = fin.is_open();
-		if (isOpen == true) {
-			
-				fin >> x;
-				fin >> y;
-				fin >> r;
-			
-		}
-		else
-		{
-			cout << "Error!" << endl;
-		}
-
-		fin.close();
-	
-	}
+	virtual void Load(string path);
 };
 
 class Rectangle : public Shape {
@@ -135,51 +59,11 @@ public:
 	Rectangle() : x(0), y(0), a(0),b(0) {}
 	Rectangle(int x, int y, int a, int b) : x(x), y(y), a(a), b(b){}
 
-	virtual void Show() const {
-		cout << "\t**Rectangle**" << endl;
-		cout << "Coords of the upper left corner: x=" << x << ", y=" << y << endl;
-		cout << "Sides of the rectangle: a=" << a <<", b="<<b<< endl;
-	}
+	virtual void Show() const;
 
-	virtual void Save(string path)   override {
+	virtual void Save(string path)   override;
 
-		ofstream fout;
-		fout.open(path);
-		bool isOpen = fout.is_open();
-		if (isOpen == true) {
-
-			fout << x << endl;
-			fout << y << endl;
-			fout << a << endl;
-			fout << b << endl;
-
-		}
-		else {
-			cout << "Error!" << endl;
-		}
-
-		fout.close();	
-	}
-
-	virtual void Load(string path) {
-		ifstream fin;
-		fin.open(path);
-		bool isOpen = fin.is_open();
-		if (isOpen == true) {
-			
-				fin >> x;
-				fin >> y;
-				fin >> a;
-				fin >> b;
-			
-		}
-		else
-		{
-			cout << "Error!" << endl;
-		}
-
-		fin.close();
-	}
+	virtual void Load(string path);
 };
 
 class Ellipse : public Shape {
@@ -192,50 +76,9 @@ public:
 	Ellipse() : x(0), y(0), a(0), b(0) {}
 	Ellipse(int x, int y, int a, int b) : x(x), y(y), a(a), b(b) {}
 
-	virtual void Show() const {
-		cout << "\t**Ellipse**" << endl;
-		cout << "Coords of the upper left corner of the rectangle circumscribed around the ellipse: x=" << 
-			x << ", y=" << y << endl;
-		cout << "Sides of the rectangle: a=" << a << ", b=" << b << endl;
-	}
+	virtual void Show() const;
 
-	virtual void Save(string path)  override  {
-		ofstream fout;
-		fout.open(path);
-		bool isOpen = fout.is_open();
-		if (isOpen == true) {
+	virtual void Save(string path)  override;
 
-			fout << x << endl;
-			fout << y << endl;
-			fout << a << endl;
-			fout << b << endl;
-
-		}
-		else {
-			cout << "Error!" << endl;
-		}
-
-		fout.close();
-	
-	}
-
-	virtual void Load(string path) {
-		ifstream fin;
-		fin.open(path);
-		bool isOpen = fin.is_open();
-		if (isOpen == true) {
-			
-				fin >> x;
-				fin >> y;
-				fin >> a;
-				fin >> b;
-			
-		}
-		else
-		{
-			cout << "Error!" << endl;
-		}
-
-		fin.close();
-	}
+	virtual void Load(string path);
 };
